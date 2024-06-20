9000b8e94d968f271fcbea0c43a19dd2abab5913
kim wonki <kimwk0229@gmail.com>
2024-06-21 01:24:32 +0900

    Spring

--- gs-rest-service-complete/src/main/java/com/example/restservice/GreetingController.java
+++ gs-rest-service-complete/src/main/java/com/example/restservice/GreetingController.java
@@ -14,7 +14,8 @@ public class GreetingController {
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "Spring") String name) {
+	public Greeting greeting(
+			@RequestParam(value = "name", defaultValue = "Spring") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 }
