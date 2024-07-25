2a5f7ffb406f2c254f59db3d08cbdece7a7d78ec
kim wonki <kimwk0229@gmail.com>
2024-07-25 20:57:20 +0900

    repo01 정리

--- gs-rest-service-complete/src/main/java/com/example/restservice/GreetingController.java
+++ gs-rest-service-complete/src/main/java/com/example/restservice/GreetingController.java
@@ -10,12 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
 	private static final String template = "Hello, %s!";
-
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/greeting")
-	public Greeting greeting(
-			@RequestParam(value = "name", defaultValue = "Spring") String name) {
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 }
