c12aaaebcd1265f2cc924dc6da720ee156f9e1a0
kim wonki <kimwk0229@gmail.com>
2024-07-26 00:04:58 +0900

    //

--- payroll/src/main/java/com/jadecross/payroll/api/EmployeeController.java
+++ payroll/src/main/java/com/jadecross/payroll/api/EmployeeController.java
@@ -23,7 +23,7 @@ class EmployeeController {
 		this.repository = repository;
 	}
 
-	@GetMapping("/employees")
+	@GetMapping("/employees")  //
 	List<Employee> all() {
 		return repository.findAll();
 	}
