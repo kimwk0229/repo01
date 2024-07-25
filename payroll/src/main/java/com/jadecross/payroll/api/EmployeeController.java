cf27a92cc27b305adcbc6eb4330a587773e76ace
kim wonki <kimwk0229@gmail.com>
2024-07-25 23:45:35 +0900

    주석 삭제

--- payroll/src/main/java/com/jadecross/payroll/api/EmployeeController.java
+++ payroll/src/main/java/com/jadecross/payroll/api/EmployeeController.java
@@ -23,17 +23,17 @@ class EmployeeController {
 		this.repository = repository;
 	}
 
-	@GetMapping("/employees")  //사원 목록
+	@GetMapping("/employees")
 	List<Employee> all() {
 		return repository.findAll();
 	}
 
-	@PostMapping("/employees")  //사원 추가
+	@PostMapping("/employees")
 	Employee newEmployee(@RequestBody Employee newEmployee) {
 		return repository.save(newEmployee);
 	}
 
-	@GetMapping("/employees/{id}")  //사원 상세 조회 11112222
+	@GetMapping("/employees/{id}")
 	Employee one(@PathVariable("id") Long id) {
 
 		return repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
